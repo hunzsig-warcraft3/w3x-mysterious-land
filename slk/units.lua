bf480f0cbc2f48e738fecf95ffa981d521fe5977
hunzsig <mzyhaohaoren@qq.com>
2020-04-15 21:44:12 +0800

    up

---
+++ slk/units.lua
@@ -0,0 +1,82 @@
+UNITS = {} --to vjass
+
+UNITS_CONF = {
+    {
+        Name = "复活石(面向东南的)",
+        abilList = "Avul",
+        file = "Doodads\\Cinematic\\RessurectionStoneSE\\RessurectionStoneSE",
+        Art = "ReplaceableTextures\\CommandButtons\\BTNResStone.blp",
+        modelScale = 1.20,
+        scale = 3.60,
+        HP = 99999,
+        spd = 0,
+        sight = 1200,
+        nsight = 1200,
+        unitSound = "",
+        weapsOn = 0,
+    },
+    {
+        Name = "复活石(面向西南的)",
+        abilList = "Avul",
+        file = "Doodads\\Cinematic\\RessurectionStoneSW\\RessurectionStoneSW",
+        Art = "ReplaceableTextures\\CommandButtons\\BTNResStone.blp",
+        modelScale = 1.20,
+        scale = 3.60,
+        HP = 99999,
+        spd = 0,
+        sight = 1200,
+        nsight = 1200,
+        unitSound = "",
+        weapsOn = 0,
+    },
+    {
+        Name = "复活阵",
+        abilList = "Avul,Aloc",
+        file = "war3mapImported\\unit_ReworkedFlameKnight.blp",
+        Art = "ReplaceableTextures\\CommandButtons\\BTNResStone.blp",
+        modelScale = 2,
+        scale = 1.00,
+        HP = 99999,
+        spd = 0,
+        sight = 1200,
+        nsight = 1200,
+        unitSound = "",
+        weapsOn = 0,
+    },
+    {
+        Name = "传送门",
+        abilList = "Avul,Aloc",
+        file = "Abilities\\Spells\\Other\\GeneralAuraTarget\\GeneralAuraTarget.mdl",
+        Art = "ReplaceableTextures\\PassiveButtons\\PASBTNElunesBlessing.blp",
+        modelScale = 2.5,
+        scale = 1.00,
+        HP = 99999,
+        spd = 0,
+        sight = 300,
+        nsight = 300,
+        unitSound = "",
+        weapsOn = 0,
+    },
+}
+
+-- units
+for _, v in ipairs(UNITS_CONF) do
+    local obj = slk.unit.ogru:new("this_units_" .. v.Name)
+    obj.Name = v.Name
+    obj.abilList = v.abilList or ""
+    obj.upgrades = ""
+    obj.file = v.file
+    obj.Art = v.Art
+    obj.modelScale = v.modelScale or 1.00
+    obj.scale = v.scale or 1.00
+    obj.HP = v.HP
+    obj.spd = v.spd or 0
+    obj.sight = v.sight
+    obj.nsight = v.nsight
+    obj.unitSound = v.unitSound or ""
+    obj.weapsOn = v.weapsOn or 0
+    obj.regenHP = 0
+    obj.regenType = ""
+    obj.fused = 0
+    table.insert(UNITS, { UNIT_ID = obj:get_id(), Name = v.Name })
+end
