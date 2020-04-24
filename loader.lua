90c537544f1884a5bdd93449e984f5599411c983
hunzsig <mzyhaohaoren@qq.com>
2020-04-25 01:13:11 +0800

    up

--- loader.lua
+++ loader.lua
@@ -126,17 +126,130 @@ end
 
 -- env岛屿环境
 local islands = {
-    { type = "sea", rect = hrect.create(-5520, -5200, 5800, 7300) }, -- 斑斓海
-    { type = "winterDeep", rect = hrect.create(-6640, 5100, 5120, 7700) }, -- 冰极雪原
-    { type = "fire", rect = hrect.create(2030, 5888, 4096, 4096) }, -- 火蛇岛
-    { type = "poor", rect = hrect.create(6970, 0, 4096, 4096) }, -- 铁环山
-    { type = "ruins", rect = hrect.create(6788, -6781, 4608, 5120) }, -- 遗迹草原
-    { type = "summer", rect = hrect.create(400, -6900, 2816, 3800) }, -- 秘潭幽林
-    { type = "dark", rect = hrect.create(7544, 6759, 3800, 4608) }, -- 枯死岸
+    {
+        name = "斑斓海",
+        rect = hrect.create(-5520, -5200, 5800, 7300),
+        env = "sea",
+        color = hColor.sea,
+        weather = nil,
+        allowWeather = {
+            { weather = "time", desc = "风光无限好" },
+            { weather = hweather.rain, desc = "忽然下起了小雨" },
+            { weather = hweather.rainstorm, desc = "下起了暴雨，波涛汹涌" },
+            { weather = hweather.snow, desc = "受到了北方冷空气的影响，奇妙地下起了雪" },
+        },
+    },
+    {
+        name = "冰极雪原",
+        rect = hrect.create(-6640, 5100, 5120, 7700),
+        env = "winterDeep",
+        color = hColor.sky,
+        weather = nil,
+        allowWeather = {
+            { weather = hweather.rain, desc = "下起了冻雨" },
+            { weather = hweather.snow, desc = "下起了雪，冰封大地" },
+            { weather = hweather.snowstorm, desc = "受到暴雪的侵袭，险象迭生" },
+            { weather = hweather.mistwhite, desc = "冰消泛起了白雾，影响了视野" },
+        },
+    },
+    {
+        name = "火蛇岛",
+        rect = hrect.create(2030, 5888, 4096, 4096),
+        env = "fire",
+        color = hColor.red,
+        weather = nil,
+        allowWeather = {
+            { weather = hweather.mistred, desc = "热火焚身，天火遍野" },
+            { weather = hweather.wind, desc = "刮起了风，带了一丝清凉" },
+        },
+    },
+    {
+        name = "铁环山",
+        rect = hrect.create(6970, 0, 4096, 4096),
+        env = "poor",
+        color = hColor.yellow,
+        weather = nil,
+        allowWeather = {
+            { weather = "time", desc = "一派大好河山的景象" },
+            { weather = hweather.rain, desc = "小雨冲刷着历史的痕迹" },
+            { weather = hweather.wind, desc = "吹起了风，遗迹更添哀愁" },
+        },
+    },
+    {
+        name = "遗迹草原",
+        rect = hrect.create(6788, -6781, 4608, 5120),
+        env = "ruins",
+        color = hColor.greenLight,
+        weather = nil,
+        allowWeather = {
+            { weather = "time", desc = "展现原野靓景" },
+            { weather = hweather.rain, desc = "小雨冲刷着历史的痕迹" },
+            { weather = hweather.rainstorm, desc = "大雨疯狂地从天而降" },
+            { weather = hweather.wind, desc = "吹起了风，遗迹更添哀愁" },
+            { weather = hweather.windstorm, desc = "狂风卷裂青空像夺命的鞭子" },
+        },
+    },
+    {
+        name = "秘潭幽林",
+        rect = hrect.create(400, -6900, 2816, 3800),
+        env = "summer",
+        color = hColor.green,
+        weather = nil,
+        allowWeather = {
+            { weather = "time", desc = "万物逗趣，生机勃勃" },
+            { weather = hweather.rain, desc = "下雨了，空气十分凉爽" },
+            { weather = hweather.rainstorm, desc = "下起了蹉跎大雨，天空乌云密布" },
+        },
+    },
+    {
+        name = "枯死岸",
+        rect = hrect.create(7544, 6759, 3800, 4608),
+        env = "dark",
+        color = hColor.purple,
+        weather = nil,
+        allowWeather = {
+            { weather = hweather.shield, desc = "可看到紫色灵魂升天的奇象" },
+            { weather = hweather.mistgreen, desc = "毒雾弥漫，注意小心" },
+            { weather = hweather.mistblue, desc = "迷惑蓝烟处处，困惑人心" },
+            { weather = hweather.rain, desc = "下起了令人生怯的小雨" },
+        },
+    },
 }
+-- 自动天气
+autoWeather = function(obj)
+    if (obj.weather ~= nil) then
+        hweather.del(obj.weather)
+    end
+    if (#obj.allowWeather > 0) then
+        local which = obj.allowWeather[math.random(1, #obj.allowWeather)]
+        if (which.weather == "time") then
+            if (his.day()) then
+                which.weather = hweather.sun
+                which.desc = "阳光灿烂，" .. which.desc
+            else
+                which.weather = hweather.moon
+                which.desc = "月色照耀，" .. which.desc
+            end
+        end
+        echo(obj.color(obj.name) .. "此时" .. which.desc)
+        obj.weather = hweather.create({
+            whichRect = obj.rect,
+            type = which.weather
+        })
+    end
+    htime.setTimeout(math.random(120, 300), function(t)
+        htime.delTimer(t)
+        autoWeather(obj)
+    end)
+end
+
 for _, v in ipairs(islands) do
-    henv.random(v.rect, v.type, nil, nil, true)
-    henv.random(v.rect, v.type, nil, nil, true)
+    henv.random(v.rect, v.env, nil, nil, false)
+    henv.random(v.rect, v.env, nil, nil, false)
+    htime.setTimeout(math.random(20, 30), function(t)
+        htime.delTimer(t)
+        autoWeather(v)
+    end)
 end
 
 -- game start(这里需要用时间事件延时N秒，不然很多动作会在初始化失效)
